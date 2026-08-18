5f4e33202ee350dc6bcffee41b4c92bd6e3d574b
Ryan <ryanfontess@hotmail.com>
2026-08-17 21:48:07 -0300

    Parte 3 finalizada

--- utils.py
+++ utils.py
@@ -6,3 +6,9 @@ def load_data(nome_arquivo):
 
     with open(caminho, "r", encoding="utf-8") as arquivo:
         return json.load(arquivo)
+
+def load_template(nome_arquivo):
+    caminho = os.path.join('static', 'templates', nome_arquivo)
+
+    with open(caminho, "r", encoding="utf-8") as arquivo:
+        return arquivo.read()
